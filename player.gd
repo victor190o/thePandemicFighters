cfef206a809475eb8a2875c685de4f3481e19d9c
victorEsantos <victor.eduardo.santos@gmail.com>
2021-04-11 12:17:23 -0300

    project

---
+++ player.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+const SPEED = 400
+const JUMPH_HEIGHT = -550
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	
+	motion.y += GRAVITY
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+		$Sprite.flip_h = false
+		$Sprite.play("run")
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("run")
+	else:
+		motion.x = 0
+		$Sprite.play("idle")
+		
+	if(is_on_floor()):
+		if(Input.is_action_pressed("ui_up")):
+			motion.y = JUMPH_HEIGHT
+	
+	motion = move_and_slide(motion, UP)
