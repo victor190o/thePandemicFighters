d5104501b5c777fc2e1539de1027703f5c0207cb
PABLAO <pablo@multipedidos.com.br>
2021-05-07 00:21:24 -0300

    adicionado fim de tiro

--- scenes/Monstro.gd
+++ scenes/Monstro.gd
@@ -1,16 +1,22 @@
 extends StaticBody2D
 
 var flip = true
+const GRAVITY = 30
 var posicao_inicial
 var posicao_final
 var velocidade = 1
 var posicao_atual
 var morto = false
 
+var motion = Vector2()
+
 func _ready():
 	$Sprite.play("walk")
 	posicao_inicial = $".".position.x
 	posicao_final = posicao_inicial + 100
+
+func _physics_process(delta):
+	motion.y += GRAVITY
 
 func _process(delta):
 	if(!morto):
--- scripts/Tiro.gd
+++ scripts/Tiro.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var velocidade = 800
 var direcao = 1
+var contato = false
 var tempo = 0
 
 func _ready():
@@ -9,11 +10,22 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	position = Vector2(position.x + velocidade * direcao * delta, position.y)
-	tempo += delta
-	if(tempo > 0.5):
-		queue_free()	
+	if(!contato):
+		$Sprite.play("tiro2")
+		position = Vector2(position.x + velocidade * direcao * delta, position.y)
+		tempo += delta
+		if(tempo > 0.5):
+			$Sprite.stop()
+			$Sprite.play("tirofim")
+			queue_free()
 
 func _on_contato_body_entered(body):
+	contato = true
+	$Sprite.stop()
+	$Sprite.play("tirofim")
 	body.dano()
+	get_node("anim").play("explosao")
 	pass # Replace with function body.
+	
+func fim():
+	queue_free()
